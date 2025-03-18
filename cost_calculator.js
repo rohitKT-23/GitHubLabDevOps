01a2fd2f478318b52f4c67c77d6b25a9fa8f77ef
rohitKT-23 <rohit.rt2004@gmail.com>
2025-03-18 15:20:04 +0530

    Fixed bug in cost calculator

---
+++ cost_calculator.js
@@ -0,0 +1,7 @@
+function calculateCloudCost() {
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+}
+
+calculateCloudCost();
