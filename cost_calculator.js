c3aec45639adb00e6b3dd47169c4899e840a6169
hlavania05 <lavaniaharshita05@gmail.com>
2025-03-18 15:42:41 +0530

    Modify js

--- cost_calculator.js
+++ cost_calculator.js
@@ -2,6 +2,12 @@ function calculateCloudCost() {
     console.log("Multi-cloud cost analytics coming soon...");
     console.log("Multi-cloud cost analytics coming soon...");
     console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
+    console.log("Multi-cloud cost analytics coming soon...");
 }
 
 calculateCloudCost();
